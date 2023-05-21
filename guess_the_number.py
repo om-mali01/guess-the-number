69095fcae7b6a044744158ad5688a8046b4f8ab6
Om Mali <133238164+om-mali01@users.noreply.github.com>
2023-05-21 11:38:44 +0530

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,26 @@
+import random
+
+number = random.randint(0, 100)
+attempts = 10
+print(f"You have {attempts} attempts to guess the number.")
+
+for chance in range(1, attempts):
+    guess = int(input("\nGuess the number: "))
+
+    if guess == number:
+        print(f"\nCongratulations! You guessed it. The number is {number}!")
+        break
+    elif guess < number:
+        print("Too low.")
+        remaining_attempts = attempts - chance
+        if remaining_attempts == 0:
+            print(f"Out of attempts. The number was {number}.")
+        else:
+            print(f"You have {remaining_attempts} attempts left.")
+    else:
+        print("Too high.")
+        remaining_attempts = attempts - chance
+        if remaining_attempts == 0:
+            print(f"Out of attempts. The number was {number}.")
+        else:
+            print(f"You have {remaining_attempts} attempts left.")
